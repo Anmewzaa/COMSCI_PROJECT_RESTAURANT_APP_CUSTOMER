1f00cbf4296772a2240fa462a274c6c089afb368
chanutkid <chanut.kidw@gmail.com>
2024-09-18 14:26:17 +0700

    add header

--- project/src/Pages/MenuPage.jsx
+++ project/src/Pages/MenuPage.jsx
@@ -1,17 +1,53 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import "../CSS/MenuPage.css";
+import Header from "../Components/Header";
+// Router DOM
 import { useSearchParams } from "react-router-dom";
+// Axois
+import axios from "axios";
 
 const MenuPage = () => {
+  const [menu, setMenu] = useState([]);
   const [searchParam] = useSearchParams();
   const id = searchParam.get("id");
   const categories = searchParam.get("categories");
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/menu/getfromcate/${categories}`)
+      .then((result) => {
+        setMenu(result.data.response);
+      });
+  }, []);
   return (
-    <div>
-      ID : {id}
-      <br />
-      CATE : {categories}
-    </div>
+    <>
+      <Header />
+      <div className="menu-box-container">
+        {/* ID : {id}
+        <br />
+        CATE : {categories} */}
+        {menu &&
+          menu.map((item) => {
+            return (
+              <>
+                <div className="menu-box">
+                  <img
+                    className="img"
+                    src={`${import.meta.env.VITE_API_URL}/images/${
+                      item.menu_image
+                    }`}
+                    alt=""
+                  />
+                  <div className="menu-name">
+                    {item.menu_name.thai}
+                    <div className="menu-price">฿ {item.menu_price}</div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+      </div>
+    </>
   );
 };
 
